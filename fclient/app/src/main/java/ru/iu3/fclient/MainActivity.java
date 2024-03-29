3947111f1bc049e86872b9573a97b9fe0d2a8db8
vpodogov <podogov@mail.ru>
2024-03-29 20:16:01 +0300

    done 3 lab

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -20,9 +20,10 @@ import java.util.Arrays;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
     ActivityResultLauncher activityResultLauncher;
+    private String pin;
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -55,21 +56,66 @@ public class MainActivity extends AppCompatActivity {
 //        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
         activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data1 = result.getData();
-                        // обработка результата
-                        String pin = data1.getStringExtra("pin");
-                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
                     }
-                });
+                }
+        );
     }
 
-    public void onButtonClick(View v)
-    {
-        Intent it = new Intent(this, PinpadActivity.class);
-        //startActivity(it);
-        activityResultLauncher.launch(it);
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void onButtonClick(View v) {
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        //startActivity(it);
+//        activityResultLauncher.launch(it);
+//        new Thread(()-> {
+//            try {
+//                byte[] trd = stringToHex("9F0206000000000100");
+//                boolean ok = transaction(trd);
+//                runOnUiThread(()-> {
+//                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+//                });
+//
+//            } catch (Exception ex) {
+//                // todo: log error
+//            }
+//        }).start();
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
     }
 
     public static byte[] stringToHex(String s) {
@@ -91,4 +137,6 @@ public class MainActivity extends AppCompatActivity {
     public static native byte[] encrypt(byte[] key, byte[] data);
 
     public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public native boolean transaction(byte[] trd);
 }
