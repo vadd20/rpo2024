990f740e26ac57935ba63f7503c13bf9c9fa8dd7
vpodogov <podogov@mail.ru>
2024-03-29 19:39:40 +0300

    done 2 lab

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,15 +1,28 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import java.util.Arrays;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -36,18 +49,46 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(res);
         System.out.println(Arrays.toString(v));
 
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
 
+//        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data1 = result.getData();
+                        // обработка результата
+                        String pin = data1.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
-    /**
-     * A native method that is implemented by the 'fclient' native library,
-     * which is packaged with this application.
-     */
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        } catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+    }
+
     public native String stringFromJNI();
+
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
+
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
 }
