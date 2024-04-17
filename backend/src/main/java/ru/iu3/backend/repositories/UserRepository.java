34c5cc0a29d612bd50a6d94fbce565957622003a
vpodogov <podogov@mail.ru>
2024-04-17 23:43:11 +0300

    done 7 lab

--- backend/src/main/java/ru/iu3/backend/repositories/UserRepository.java
+++ backend/src/main/java/ru/iu3/backend/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package ru.iu3.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.iu3.backend.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByToken(String token);
+
+    Optional<User> findByLogin(String login);
 }
