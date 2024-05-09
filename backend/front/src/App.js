92d0f6c230e6959bc173d942774de1f8a68d34ca
Vadim Podogov <VPodogov@croc.ru>
2024-05-09 19:11:03 +0300

    done 10 lab

--- backend/front/src/App.js
+++ backend/front/src/App.js
@@ -1,27 +1,54 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.css';
+import React from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavigationBar from "./components/NavigationBarClass";
 import Home from "./components/Home";
 import Another_Home from "./components/Another_Home";
 import Login from "./components/Login";
+import Utils from "./utils/Utils";
 
-function App() {
+import {connect} from "react-redux";
+
+const ProtectedRoute = ({children}) => {
+    let user = Utils.getUser();
+    return user ? children : <Navigate to={'/login'}/>
+};
+
+function App(props) {
     return (
         <div className="App">
             {/* Смысл в том, что браузер привязывает компоненты к локальным путям внутри приложения */}
             <BrowserRouter>
                 <NavigationBar/>
-                <div className="container-fluid">
+                <div className="container-fluid"> {
+                    props.error_message &&
+                    <div className="alert alert-danger m-1">{props.error_message}</div>
+                }
                     <Routes>
-                        <Route path="home" element={<Home/>}/>
-                        {/* Дополнение: создал новый компонент, чтобы разделить то, что выводится с каждой из ссылок */}
-                        <Route path="Another_Home" element={<Another_Home/>}/>
                         <Route path="login" element={<Login/>}/>
+                        {/* Не забывай про свою вторую страницу Another Home*/}
+                        <Route path="Another_home" element={
+                            <ProtectedRoute>
+                                <Another_Home/>
+                            </ProtectedRoute>
+                        }/>
+
+                        <Route path="home" element={
+                            <ProtectedRoute>
+                                <Home/>
+                            </ProtectedRoute>}/>
                     </Routes>
                 </div>
             </BrowserRouter>
         </div>
     );
 }
-export default App;
+
+const mapStateToProps = function (state) {
+    const {msg} = state.alert;
+    return {error_message: msg};
+}
+
+export default connect(mapStateToProps)(App);
--- backend/front/src/components/Login.jsx
+++ backend/front/src/components/Login.jsx
@@ -1,56 +1,55 @@
 import React, {useState} from "react";
 import BackendService from "../services/BackendService";
-import Utils from "../utils/Utils";
 import {useNavigate} from "react-router-dom";
 
+import {connect} from "react-redux";
+import {store, userActions} from "../utils/Rdx";
+
 // Форма авторизации пользователя
-export default function Login() {
+export default connect()(function Login() {
     // Здесь используется хук useState - позволяет сохранять и изменять переменные состояния так, как если бы они
     // были компонентами класса
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loggingIn, setLoggingIn] = useState(false);
     const [submitted, setSubmitted] = useState(false);
-    const [error_message, setErrorMessage] = useState(null);
+    // const [error_message, setErrorMessage] = useState(null);
 
     // Данный хук позволяет осуществлять навигацию между компонентами классов
     const nav = useNavigate();
-
     function handleChangeLogin(e) {
         setUsername(e.target.value);
     }
-
     function handleChangePassword(e) {
         setPassword(e.target.value);
     }
-
     // Если всё обработано хорошо, то переходим на страницу /home
     function handleSubmit(e) {
         e.preventDefault();
         setSubmitted(true);
-        setErrorMessage(null);
+        // setErrorMessage(null);
         setLoggingIn(true);
         BackendService.login(username, password)
             .then(resp => {
                 console.log(resp.data);
-                Utils.saveUser(resp.data);
                 setLoggingIn(false);
+                store.dispatch(userActions.login(resp.data))
                 nav("/home");
             })
             .catch(err => {
-                if (err.response && err.response.status === 401)
-                    setErrorMessage("Ошибка авторизации");
-                else
-                    setErrorMessage(err.message);
-                setLoggingIn(false);
+                // if (err.response && err.response.status === 401)
+                //     setErrorMessage("Ошибка авторизации");
+                // else
+                //     setErrorMessage(err.message);
+                // setLoggingIn(false);
             })
     }
 
     // В качестве возвращаемого значения можно использовать форму авторизации (то, что нам нужно)
     return (
         <div className="col-md-6 me-0">
-            {error_message &&
-                <div className="alert  alert-danger mt-1 me-0 ms-0">{error_message}</div>}
+            {/*error_message &&*/}
+            {/*    <div className="alert  alert-danger mt-1 me-0 ms-0">{error_message}</div>*/}
             <h2>Вход</h2>
             <form name="form" onSubmit={handleSubmit}>
 
@@ -60,7 +59,6 @@ export default function Login() {
                            name="username" value={username} onChange={handleChangeLogin}/>
                     {submitted && !username && <div className="help-block text-danger">Enter your name</div>}
                 </div>
-
                 <div className="form-group">
                     <label htmlFor="password">Пароль</label>
                     <input type="password"
@@ -68,7 +66,6 @@ export default function Login() {
                            name="password" value={password} onChange={handleChangePassword}/>
                     {submitted && !password && <div className="help-block text-danger">Enter your password</div>}
                 </div>
-
                 <div className="form-group mt-2">
                     <button className="btn btn-primary">
                         {loggingIn && <span className="spinner-border spinner-border-sm" role="status"
@@ -76,8 +73,7 @@ export default function Login() {
                         Войти
                     </button>
                 </div>
-
             </form>
         </div>
     );
-}
+})
